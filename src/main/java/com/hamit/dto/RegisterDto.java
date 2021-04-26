d5672f8a58b897849322e034437aa8af2f35334c
HamitMizrak <hamitmizrak@gmail.com>
2021-04-27 02:17:49 +0300

    Lombok

--- src/main/java/com/hamit/dto/RegisterDto.java
+++ src/main/java/com/hamit/dto/RegisterDto.java
@@ -1,8 +1,12 @@
 package com.hamit.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterDto {
 	// Repository https://github.com/hamitmizrak/BlogProject.git
 	private long id;
