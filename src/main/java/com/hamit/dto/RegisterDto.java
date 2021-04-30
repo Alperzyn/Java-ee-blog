a54788ad4003e5b3352570f0c9a55513191bd3d2
HamitMizrak <hamitmizrak@gmail.com>
2021-04-30 18:35:29 +0300

    RegisterDto RegisterEntity Form

--- src/main/java/com/hamit/dto/RegisterDto.java
+++ src/main/java/com/hamit/dto/RegisterDto.java
@@ -1,14 +1,27 @@
+
 package com.hamit.dto;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class RegisterDto {
-	//Repository https://github.com/hamitmizrak/Java-ee-blog.git
-	private long id;
+@Named(value = "registerDto")
+@ApplicationScoped
+@Data @AllArgsConstructor @NoArgsConstructor
+
+public class RegisterDto implements Serializable {
+
+	private static final long serialVersionUID = -6051380484920087806L;
+	private long registerId;
+	private String registerName;
+	private String registerSurname;
+	private String registerEmail;
+	private String registerPassword;
+	private java.util.Date registerCreationDate;
 
 }
