dc51b33a353fbc7b59d35a58d1059c8a2ab4caed
HamitMizrak <hamitmizrak@gmail.com>
2021-05-07 21:58:50 +0300

    input ve commandButton commandLink link button

--- src/main/java/com/ecodation/dersler/cdi/A011_Input.java
+++ src/main/java/com/ecodation/dersler/cdi/A011_Input.java
@@ -6,12 +6,14 @@ import javax.inject.Named;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
 
 //cdi
 @Named(value = "input")
 @ApplicationScoped
 
 // lombok
+@Log
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,9 +22,23 @@ public class A011_Input {
 	private long id;
 	private String name = "";
 	private String surname = "";
+	private String password;
+	private String form;
 
 	public String adiSoyadiMethod() {
+		System.out.println(this.id + " " + this.name + " " + this.surname + " " + this.password);
+		log.info(this.id + " " + this.name + " " + this.surname + " " + this.password);
+		log.warning(this.id + " " + this.name + " " + this.surname + " " + this.password);
 		return "Ders_008_Input.xhtml?faces-redirect=true";
+	}
+
+	// ayný sayfada kalmak icin
+	public String adiSoyadiMethod2() {
+		return null;
+	}
+
+	// ayný sayfada kalmak icin
+	public void adiSoyadiMethod3() {
 	}
 
 }
