925b4ca0f2298841a7b96cb47efa3e2c96854195
HamitMizrak <hamitmizrak@gmail.com>
2021-05-16 16:31:54 +0300

    passing parameters-3

--- src/main/java/com/examples/jsf/A015_PassingParameter.java
+++ src/main/java/com/examples/jsf/A015_PassingParameter.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 
 import lombok.Data;
@@ -13,19 +14,27 @@ import lombok.extern.java.Log;
 @ApplicationScoped
 @Log
 @Data
-public class A015_Karar2 {
+public class A015_PassingParameter {
 	
-	// Param -1
+	// Param -1 action Methodd ile
 	public void passing1(String pass1) {
 		System.out.println("Gelen değer: " + pass1);
+		log.warning(pass1 + " ");
 		// Db alanı3
 	}
 	
-	// Param-2
+	// Param-2 action /servlet ile
 	public void passing2() {
 		Map<String, String> gelenParam = FacesContext.getCurrentInstance().getExternalContext()
 				.getRequestParameterMap();
-		String veri = gelenParam.get("key");
+		String veri = gelenParam.get("key2");
 		log.warning(veri + " ");
+	}
+	
+	// Param-3 actionListener
+	public void passing3(ActionEvent actionEvent) {
+		System.out.println(actionEvent.getComponent().getAttributes().get("key3"));
+		log.warning(actionEvent.getComponent().getAttributes().get("key3") + " ");
+		
 	}
 }
